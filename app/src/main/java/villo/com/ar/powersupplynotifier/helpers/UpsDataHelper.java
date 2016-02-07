9a9402d842a6d6d5055b2bb9880c45915bff8536
Gabriel Villoldo <gabrielvilloldo@gmail.com>
2016-02-07 12:55:16 -0300

    Addded support for SSL (with self-signed certificate override). Updated okHttp library

--- app/src/main/java/villo/com/ar/powersupplynotifier/helpers/UpsDataHelper.java
+++ app/src/main/java/villo/com/ar/powersupplynotifier/helpers/UpsDataHelper.java
@@ -16,9 +16,17 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.Proxy;
+import java.security.cert.CertificateException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import villo.com.ar.powersupplynotifier.Constants;
 import villo.com.ar.powersupplynotifier.model.UpsCallback;
@@ -31,9 +39,18 @@ import villo.com.ar.powersupplynotifier.model.UpsValues;
 public class UpsDataHelper {
 
     public static void fetchNewValues(final Context context, final UpsCallback upsCallback) {
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client;
+
         final SharedPreferences sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(context);
+        String url = sharedPreferences.getString("general_router_url", "");
+
+        if (url.toLowerCase().startsWith("https")) {
+            client = getUnsafeOkHttpClientForHttps();
+        } else {
+            client = getRegularOkHttpClient();
+        }
+
         client.setAuthenticator(new Authenticator() {
             @Override
             public Request authenticate(Proxy proxy, Response response) throws IOException {
@@ -49,8 +66,6 @@ public class UpsDataHelper {
                 return null;
             }
         });
-
-        String url = sharedPreferences.getString("general_router_url", "");
 
         Request request = new Request.Builder().url(url).build();
 
@@ -154,6 +169,51 @@ public class UpsDataHelper {
             return null;
         }
         return response;
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClientForHttps() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            okHttpClient.setSslSocketFactory(sslSocketFactory);
+            okHttpClient.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static OkHttpClient getRegularOkHttpClient() {
+        return new OkHttpClient();
     }
 
 }
